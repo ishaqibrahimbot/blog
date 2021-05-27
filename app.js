b49b3a08cc01bec0258fed41d43afec9450de0a6
Ishaq Ibrahim <ishaqibrahimbss@gmail.com>
2021-05-27 21:31:56 +0500

    Updated blog functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,8 +16,51 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+//GLobal variables
+let posts = [];
 
+//Root Route
+app.get("/", (req, res) => {
+  res.render("home", {homeContent: homeStartingContent, posts: posts});
+});
 
+//Contact Route
+app.get("/contact", (req, res) => {
+  res.render("contact", {contactContent: contactContent});
+});
+
+//About Route
+app.get("/about", (req, res) => {
+  res.render("about", {aboutContent: aboutContent});
+});
+
+//Compose Route
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+//Using Express Route Parameters
+app.get("/posts/:postID/", (req, res) => {
+  let matchFound = false;
+  let query = _.kebabCase(req.params.postID);
+  posts.forEach((item) => {
+    if (query === _.kebabCase(item.title)) {
+      res.render("post", {postTitle: item.title, postBody: item.content});
+    }
+  });
+});
+
+app.post("/compose", (req, res) => {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(post);
+
+  res.redirect('/');
+
+});
 
 
 
