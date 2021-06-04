28a399c61e707ae94b2cb6a91d7c17cb2f633d61
Ishaq Ibrahim <ishaqibrahimbss@gmail.com>
2021-06-04 12:07:42 +0500

    Integrate MongoDB database

--- app.js
+++ app.js
@@ -4,6 +4,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+
+const getDate = function () {
+  let date = new Date();
+  return date.toLocaleDateString();
+}
+
+mongoose.connect("mongodb+srv://admin-ishaq:g8wnvw57dBiX2z6@ishaqclusterone.n3mde.mongodb.net/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const blogSchema = new mongoose.Schema ({
+  title: String,
+  date: String,
+  content: String
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,12 +33,14 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-//GLobal variables
-let posts = [];
-
 //Root Route
 app.get("/", (req, res) => {
-  res.render("home", {homeContent: homeStartingContent, posts: posts});
+
+  Blog.find({}, (err, blogList) => {
+    if (!err) {
+      res.render("home", {homeContent: homeStartingContent, posts: blogList});
+    }
+  });
 });
 
 //Contact Route
@@ -34,41 +53,33 @@ app.get("/about", (req, res) => {
   res.render("about", {aboutContent: aboutContent});
 });
 
-//Compose Route
+//Compose Route GET and POST
 app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
+app.post("/compose", (req, res) => {
+
+  const blogPost = new Blog ({
+    title: req.body.postTitle,
+    date: getDate(),
+    content: req.body.postBody
+  });
+
+  blogPost.save();
+  res.redirect('/');
+});
+
 //Using Express Route Parameters
 app.get("/posts/:postID/", (req, res) => {
-  let matchFound = false;
-  let query = _.kebabCase(req.params.postID);
-  posts.forEach((item) => {
-    if (query === _.kebabCase(item.title)) {
-      res.render("post", {postTitle: item.title, postBody: item.content});
+  const postID = req.params.postID;
+
+  Blog.findById(postID, (err, post) => {
+    if (!err) {
+      res.render("post", {postTitle: post.title, postBody: post.content});
     }
   });
 });
-
-app.post("/compose", (req, res) => {
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  };
-
-  posts.push(post);
-
-  res.redirect('/');
-
-});
-
-
-
-
-
-
-
-
 
 
 
